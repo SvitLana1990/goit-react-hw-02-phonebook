ab18f8515cbf360f907026fae64d9ee275250cc8
SvitLana1990 <svitlana112@outlook.es>
2023-11-02 13:06:20 +0100

    fix-start

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,6 @@ import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
 body {
-  background-color: ${p => p.theme.colors.beige};
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { GlobalStyle } from './GlobalStyle';
+import { GlobalStyle } from '../GlobalStyle';
 
 export const App = () => {
   return (
